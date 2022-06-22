2e14fed5ba76879aa640c632d920057f7afcc35c
Jeremy Tesh <jermitesh20@gmail.com>
2022-06-22 12:20:24 +0300

    repo setup

---
+++ scripts/file_handler.py
@@ -0,0 +1,12 @@
+import json
+import pandas as pd
+
+
+
+class FileHandler():
+    """
+        Read file and save file from different to some format
+    """
+
+    def __init__(self):
+        self._loo
